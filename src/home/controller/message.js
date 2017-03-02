df2bfa1beb16df713bd612e6fd058f98a9e126a3
zhanghan <izhanghan@outlook.com>
2017-03-02 22:20:13 +0800

    message改动

--- src/home/controller/message.js
+++ src/home/controller/message.js
@@ -4,28 +4,28 @@ import Base from './base.js';
 
 export default class extends Base {
   /**
-	 * 代码即注释
+   * 代码即注释
    * index action
    * @return {Promise} []
    */
-  
-    async readmessageAction() {
-        let msgId = this.post('msgId')
-        // 设置成已读
-        await this.model('message').where({id: msgId}).update({is_read: 1})
-	}
 
-    /**
-     * 
-     */
-    async getMessageAction() {
-        let user = await this.model('user').findById(await this.session('uid'))
-		    let message = user.message
-		}
+  async readmessageAction() {
+    let msgId = this.post('msgId')
+      // 设置 成已读; 
+    await this.model('message').where({ id: msgId }).update({ is_read: 1 })
+  }
 
-    // async getMessageAction() {
-		// 	let msgId = this.post('msgId')
-		// 	// 设置成已读
-		// 	await this.model('message').where({id: msgId}).update({is_read: 1})
-		// }
+  /**
+   * 
+   */
+  async getMessageAction() {
+    let user = await this.model('user').findById(await this.session('uid'))
+    let message = user.message
+  }
+
+  // async getMessageAction() {
+  // 	let msgId = this.post('msgId')
+  // 	// 设置成已读
+  // 	await this.model('message').where({id: msgId}).update({is_read: 1})
+  // }
 }
