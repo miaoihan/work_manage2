86340a8da767a66a9de6d21c0286bfca62c2e66e
zhanghan <izhanghan@outlook.com>
2017-04-29 12:19:44 +0800

    update sql

---
+++ work_manage_17_4_29.sql
@@ -0,0 +1,178 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50716
+ Source Host           : localhost
+ Source Database       : work_manage
+
+ Target Server Type    : MySQL
+ Target Server Version : 50716
+ File Encoding         : utf-8
+
+ Date: 04/29/2017 12:19:32 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `answer`
+-- ----------------------------
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_id` int(11) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL,
+  `scored` int(11) DEFAULT NULL,
+  `commit_state` int(11) DEFAULT '1' COMMENT '0. 未提交、为批改 1：已提交、未批改 2：已提交、已批改',
+  `content_html` text,
+  `content_md` text,
+  `created_time` bigint(20) NOT NULL,
+  `del_state` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) DEFAULT NULL,
+  `a_id` int(11) DEFAULT NULL,
+  `content_html` text,
+  `content_md` text,
+  `created_time` bigint(20) NOT NULL,
+  `del_state` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `message`
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `to` int(11) DEFAULT NULL,
+  `from` int(11) DEFAULT NULL,
+  `content` text,
+  `link` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `is_read` varchar(255) DEFAULT NULL,
+  `created_time` bigint(20) DEFAULT NULL,
+  `del_state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `question`
+-- ----------------------------
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) DEFAULT NULL,
+  `a_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content_html` text,
+  `content_md` text,
+  `level` int(11) NOT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `is_good` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否为优秀作业',
+  `created_time` bigint(20) NOT NULL,
+  `del_state` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `question`
+-- ----------------------------
+BEGIN;
+INSERT INTO `question` VALUES ('17', null, null, 'ceshi', '<p>测试</p>\r\n', '测试', '1', null, '0', '1493439513802', '0'), ('18', null, null, '任务', '<p>任务2</p>\r\n', '任务2', '2', null, '0', '1493439526018', '0');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `right`
+-- ----------------------------
+DROP TABLE IF EXISTS `right`;
+CREATE TABLE `right` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `role`
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL COMMENT '0: 系统管理员 1: 普通管理员 2: 毕业用户 3: 学员 4: 非学员用户 5: 游客',
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `createAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `role`
+-- ----------------------------
+BEGIN;
+INSERT INTO `role` VALUES ('1', '0', '系统管理员', 'xtgly', null), ('2', '1', '普通管理员', 'ptgly', null), ('3', '2', '毕业用户', 'byyh', null), ('4', '3', '普通学员', 'ptxy', null), ('5', '4', '非学员用户', 'fxyyh', null), ('6', '5', '游客', 'yk', null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `session`
+-- ----------------------------
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cookie` varchar(255) NOT NULL DEFAULT '',
+  `data` text,
+  `expire` bigint(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cookie` (`cookie`),
+  KEY `expire` (`expire`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `session`
+-- ----------------------------
+BEGIN;
+INSERT INTO `session` VALUES ('71', 'rSxX51nTg3tzPZ51r38TIF3qjiClM2N3', '{\"uid\":41}', '1493525931418');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `realname` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `role_id` int(11) NOT NULL DEFAULT '4' COMMENT '0:系统管理员 1: 普通管理员 2: 毕业用户 3：学员 4：非学员用户  5: 游客',
+  `rightnames` varchar(255) DEFAULT NULL,
+  `cansees` varchar(255) DEFAULT NULL COMMENT '可查看的问题id',
+  `has_answer` varchar(255) NOT NULL DEFAULT '' COMMENT '回答的题号（级别）',
+  `pass_time` int(11) NOT NULL DEFAULT '0' COMMENT '回答通过的次数',
+  `level` int(11) NOT NULL,
+  `created_time` bigint(20) NOT NULL,
+  `del_state` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES ('41', null, '994296086@qq.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'han', null, '//gravatar.duoshuo.com/avatar/4e0d6a59c203498c032e91741df811e6?s=100&r=G&d=retro', '0', null, '', '', '0', '0', '1493439084847', '0');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
